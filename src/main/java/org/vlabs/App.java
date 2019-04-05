cbebd6d0ffe0574a0521af85900e0fd617253284
ariyue <ariyue@163.com>
2019-04-06 00:48:15 +0800

    .

--- src/main/java/org/vlabs/App.java
+++ src/main/java/org/vlabs/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! i am lily" );
+        System.out.println( "Hello World! i am lily, dev in king branch" );
     }
 }
